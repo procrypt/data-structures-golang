97aff216adf55fd0b86da2291965b6682564da43
Abhishek <abhishek@linux.com>
2018-04-01 01:01:03 -0400

    find missing element in an array using hash table

--- missing-element/using-hash-table.go
+++ missing-element/using-hash-table.go
@@ -4,12 +4,12 @@ import "fmt"
 
 func main() {
 
-	a := []int{1,2,3,4,5,6,7,8}
-	b := []int{1,3,4,5,6,8,2}
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	b := []int{1, 3, 4, 5, 6, 8, 2}
 
 	c := make(map[int]int)
 
-	for _,i := range a {
+	for _, i := range a {
 		c[i] += 1
 	}
 
@@ -17,7 +17,7 @@ func main() {
 		c[j] -= 1
 	}
 
-	for k,_ := range c {
+	for k, _ := range c {
 		if c[k] != 0 {
 			fmt.Println(k)
 		}
