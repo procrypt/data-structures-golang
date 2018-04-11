c8630a51589618ecb1e818309e680924fa848403
Abhishek <abhishek@linux.com>
2018-04-11 12:49:13 -0400

    doubly linked list implementation in go

---
+++ doubly-linked-list/doubly-linked-list.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+type element struct {
+	value interface{}
+	next *element
+	prev *element
+}
+
+type List struct {
+	first *element
+	last *element
+	size int
+}
+
+func New() *List {
+	return &List{}
+}
+
+func(list *List) Add(values ...interface{}) {
+	for _, value := range values {
+		newElement := &element{value: value}
+		if list.size == 0 {
+			list.first = newElement
+			list.last = newElement
+		} else {
+			list.last.next = newElement
+			newElement.prev = list.last
+			list.last = newElement
+		}
+		list.size++
+	}
+}
+
+func main() {
+
+	a := New()
+	a.Add("Abhishek","Pratap","Singh")
+
+	fmt.Println("+++Forward+++")
+
+	fmt.Println(a.first.value)
+	fmt.Println(a.first.next.value)
+	fmt.Println(a.first.next.next.value)
+
+	fmt.Println("+++Backward+++")
+	fmt.Println(a.first.next.value)
+	fmt.Println(a.first.next.prev.value)
+	fmt.Println(a.first.next.next.value)
+	fmt.Println(a.first.next.next.prev.value)
+}
